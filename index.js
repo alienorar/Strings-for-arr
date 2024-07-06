75c6ef5ef39b56c52fbe94b80112c6485091316d
alienorar <rayhonakhayrullaeva@gmail.com>
2024-07-06 14:22:24 +0500

    first commit

--- index.js
+++ index.js
@@ -1,182 +1,59 @@
 // Task 1
 
-// function findPrime() {
-//     let numbers = [2, 4, 1, 6, 5]
-//     let new_array = []
+// function getInitialOdds(number) {
+//     let result =[]
+//     let initialOdds = 1;
+//     for (let i = 0; i < number; i++) {
+//       result.push(initialOdds)
+//         initialOdds += 2
 
-//     for (let i = 0; i < numbers.length; i++) {
-//         let count = 0;
-//         for (let g = 1; g < numbers[i]; g++) {
-
-//             if (numbers[i] % g == 0) {
-//                 count++
-
-//             }
-
-
-//         }
-
-//         if (count === 1) {
-//             new_array.push(numbers[i])
-//             console.log(new_array);
-//         }
-//         else {
-//             console.log("There is no prime numbers");
-//         }
 //     }
-
+//  return result
 // }
-// findPrime()
-
+// console.log(getInitialOdds(4));
 
 // Task 2
 
-// function findMaxNum(...nums) {
-
-//     let max = nums[0]
-//     for (let index in nums) {
-// if (nums[index] > max) {
-//     max = nums[index];
-   
+// function findVowels(str) {
+//     let result = []
+//     let countVowel = 0;
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i].toLowerCase() == "a" || str[i].toLowerCase() == "e" || str[i].toLowerCase() == "i" || str[i].toLowerCase() == "o" || str[i].toLowerCase() == "u" || str[i].toLowerCase() == "o'") {
+//     result.push(str[i])
+//     countVowel ++
+//     countVowel 
 // }
-//     }
-//     console.log(max);  
 // }
-// findMaxNum( 4, 5, 6, 7)
+// console.log(`There are ${countVowel} voiwels in string and they are ${result}`);
+// console.log(result);
+// }
+// findVowels("helolololo")
 
 
 // Task 3
 
-// function findMinNum(...nums) {
-
-//     let min = nums[0]
-//     for (let index in nums) {
-// if (nums[index] < min) {
-//     min = nums[index];
-   
-// }
-//     }
-//     console.log(min);  
-// }
-// findMinNum( 4, 5, 6, 7)
-
-
-// Task 4
-
-// let array = [2,4,6,3,2,4]
-// let duplicated_array =[]
-
-// function findDuplicate() {
-//   for (let i = 0; i < array.length; i++) {
-   
-//   if (!duplicated_array.includes(array[i])) {
-//       duplicated_array.push(array[i])
-//   }
-
-//   }
-//     console.log(duplicated_array); 
-// }
-// findDuplicate()
-
-
-// Task 5
-
-// function findLetter(str) {
-//     let count_letter = 0;
-//     for (let i = 0; i < str.length; i++) {
-//        if (str[i].toLowerCase() == "a") {
-//         count_letter ++
-//        }
-        
-//     }
-//     console.log(count_letter);
-// }
-// findLetter("Atmosphere")
-
-// Task 6
-
-// function countTotalSum (...array) {
+// function countTotalSum(...array) {
 //     let max = array[0]
 //     let min = array[0]
-//      for (let index in array) {
-// if (array[index] > max) {
-//     max = array[index];
- 
-// }
-//          if (array[index] < min) {
-//              min = array[index];
 
-//          }
+//     for (let index in array) {
+//         if (array[index] > max) {
+//             max = array[index];
+
+//         }
+//         if (array[index] < min) {
+//             min = array[index];
+
+//         }
+
 //     }
-
-//     console.log(max);
-//     console.log(array.indexOf(max));
-//     array.splice(4,1,1)
 //     console.log(array);
-
-//     console.log(min);
-//     console.log(array.indexOf(min));
-//     array.splice(1,1,4) 
-//     console.log(array);
+//     console.log(`Maximum number is ${max} in array `);
+//     array.splice(4, 1, 1)
+//     console.log(`Minimum number is ${min} in array `);
+//     array.splice(1, 1, 8)
+//     console.log(`Replaced array is [${array}]`);
 
 // }
-// countTotalSum(2,1,4,3,8)
+// countTotalSum(2, 1, 4, 3, 8)
 
-
-// Task 1
-
-
-// function reverseName(str) {
-// let reversedname =""
-//     for (let i = str.length -1; i >= 0; i--) {
-//        reversedname += str[i]
-
-//     } 
-//     console.log(reversedname);  
-// }
-// reverseName("Rayhona")
-
-
-// Task 2
-
-// let array = [
-// [2,3,4,5],
-// [3,4,2],
-// [2,3]
-// ]
-
-// function totalSum(array=[]) {
-//     let total = 0;
-//     for (let i = 0; i < array.length; i++) {
-//        for (let g = 0; g < array[i].length; g++) {
-//        total +=array[i][g];
-//        }
-        
-//     }
-//     console.log(total);
-// }
-// totalSum(array)
-
-// Task 3
-// const array = [" hello ", " worldd ", " now i am taking It course "]
-
-//  function maxStr (array = []) {
-//      let max = array[0].length
-// let index = 0
-//      for (let i in array.length) {
-//          if (array[i].length > max) {
-//     max = array[i].length;
-//  index = i
-// }}
-//      console.log(max);
-//          console.log(index);
-// }
-// maxStr(array)
-
-
-let text = String.fromCharCode(
-    113, 111, 121, 105, 108, 32, 115, 122, 32, 116, 111, 112, 116, 105, 110, 103,
-    105, 122, 32, 55357,
-);
-
-console.log(text);
